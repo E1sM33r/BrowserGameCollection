0ab887ea370fb9f2941a07e9b6efb9a292247bf7
Marcel <marcel.sippel@iem.thm.de>
2020-12-27 16:00:57 +0100

    Passwort Mindestlänge ergänzt
    Passwort Mindestlänge von 8 Zeichen ergänzt

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -25,7 +25,7 @@ class RegisterController extends Controller
             'name' => 'required|max:255',
             'username' => 'required|max:255',
             'email' => 'required|email|max:255',
-            'password' => 'required|confirmed',
+            'password' => 'required|min:8|confirmed',
         ]);
 
         User::create([
