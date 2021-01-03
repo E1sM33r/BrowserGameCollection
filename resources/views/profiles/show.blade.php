500d9a738076c26960d765c0f94f15e5016e4e06
Marcel <marcel.sippel@iem.thm.de>
2021-01-03 13:43:39 +0100

    Highscores auf Nutzerprofil anzeigen

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -17,16 +17,16 @@
 
                 @can('update', $user->profile)
                     <div>
-                    <a href="/account/{{$user->id}}/edit">
-                        <div class="bg-gray-200 rounded-lg p-1 m-1">
-                            Account bearbeiten
-                        </div>
-                    </a>
-                    <a href="/profile/{{$user->id}}/edit">
-                        <div class="bg-gray-200 rounded-lg p-1 m-1 text-center">
-                            Profil bearbeiten
-                        </div>
-                    </a>
+                        <a href="/account/{{$user->id}}/edit">
+                            <div class="bg-gray-200 rounded-lg p-1 m-1">
+                                Account bearbeiten
+                            </div>
+                        </a>
+                        <a href="/profile/{{$user->id}}/edit">
+                            <div class="bg-gray-200 rounded-lg p-1 m-1 text-center">
+                                Profil bearbeiten
+                            </div>
+                        </a>
                     </div>
                 @endcan
             </div>
@@ -37,27 +37,49 @@
                     <img src="/storage/{{ $user->profile->image }}" class="shadow rounded-full max-w-full h-auto align-middle border-none">
                 </div>
                 <div class="w-3/4 bg-gray-100 shadow rounded-lg p-3 flex flex-col justify-between">
-                    <div>
-                        <div class="text-lg px-3">
-                            <span class="font-medium">Name: </span>{{ $user->name }} <br/>
+                    <div class="flex justify-between">
+                        <div class="w-1/2">
+                            <div class="text-lg px-3">
+                                <span class="font-medium">Name: </span>{{ $user->name }} <br/>
+                            </div>
+                            <div class="py-5">
+                                <div class="font-bold text-2xl p-3">
+                                    Beschreibung
+                                </div>
+                                <div class="px-3">
+                                    {{ $user->profile->description }}
+                                </div>
+                            </div>
                         </div>
-                        <div class="py-5">
-                            <div class="font-bold text-2xl p-3">
-                                Beschreibung
-                            </div>
-                            <div class="px-3">
-                                {{ $user->profile->description }}
-                            </div>
+                        <div class="flex flex-col items-center w-1/2">
+                            <p class="pb-2 font-medium text-lg">Highscores</p>
+                            @if($user->highscores->count()>0)
+                                <table class="w-3/4">
+                                    <tr>
+                                        <th class="border border-black">Spiel</th>
+                                        <th class="border border-black">Score</th>
+                                        <th class="border border-black">Rang</th>
+                                    </tr>
+
+                                    @for($i = 0; $i<$user->highscores->count(); $i++)
+                                        <tr>
+                                            <td class="text-center border border-black"><a href="{{ route('game.show', $user->highscores[$i]->game->id) }}">{{ $user->highscores[$i]->game->title }}</a></td>
+                                            <td class="text-center border border-black">{{ $user->highscores[$i]->score }}</td>
+                                            <td class="text-center border border-black">{{ $user->getHighscoreRank($user->highscores[$i]->game) }}</td>
+                                        </tr>
+                                    @endfor
+
+                                </table>
+                            @else
+                                Keine Highscores vorhanden
+                            @endif
                         </div>
                     </div>
-                    <div>
+                    <div class="px-4">
                         Mitglied seit {{ $user->created_at->format('d/m/Y') }}
                     </div>
                 </div>
-
             </div>
-
-
         </div>
     </div>
 @endsection
