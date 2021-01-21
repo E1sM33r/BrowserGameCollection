205edd19117525f93fee8e06053ae3e5fb63d29a
Marcel <marcel.sippel@iem.thm.de>
2021-01-21 15:55:21 +0100

    Fehlermeldungen übersetzt

---
+++ resources/lang/de/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Dein Passwort wurde zurückgesetzt',
+    'sent' => 'Wir haben dir eine E-Mail mit dem Zurücksetzungslink geschickt!',
+    'throttled' => 'Bitte warte bevor du es erneut versuchst.',
+    'token' => 'Das Passwort Token ist ungültig.',
+    'user' => "Wir konnten keinen Benutzer mit dieser E-Mail Adresse finden.",
+
+];
--- resources/lang/de/validation.php
+++ resources/lang/de/validation.php
@@ -1,5 +1,13 @@
 <?php
 
 return [
-    'required' => ':attribute ist erfordert.',
+    'required' => 'Dieses Feld ist erfordert.',
+    'confirmed' => 'Die Passwörter stimmen nicht überein.',
+    'unique' => 'Nicht mehr verfügbar.',
+    'min' => [
+        'numeric' => 'Das Passwort muss mindestens :min Zeichen lang sein.',
+        'file' => 'The :attribute must be at least :min kilobytes.',
+        'string' => 'Das Passwort muss mindestens :min Zeichen lang sein.',
+        'array' => 'The :attribute must have at least :min items.',
+    ],
 ];
